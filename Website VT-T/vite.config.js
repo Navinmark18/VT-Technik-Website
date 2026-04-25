b45f0c9c353c1837f52a220c450288f04f02f0c5
Navinmark18 <96864926+Navinmark18@users.noreply.github.com>
2026-04-25 15:58:35 +0000

    b

--- Website VT-T/vite.config.js
+++ Website VT-T/vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
+  plugins: [
+    basicSsl()
+  ],
   server: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: [
-      'eventvin.de',
-      'www.eventvin.de'
-    ],
+    allowedHosts: ['eventvin.de'],
     proxy: {
       '/api': {
         target: 'http://localhost:4000',
